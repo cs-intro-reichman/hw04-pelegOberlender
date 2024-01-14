862aa5829e8b051a36db6b273f0112df5c3ffa32
פלג אוברלנדר <pelegoberlender@h-MacBook-Air-sl-plg.local>
2024-01-14 18:25:40 +0200

    new

--- StringOps.java
+++ StringOps.java
@@ -22,15 +22,41 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
+        System.out.println(capVowelsLowRest("One two tHRee world"));
         
     }
 
     public static String capVowelsLowRest (String string) {
-                return "";
+        String newString = "";
+        for(int i = 0; i < string.length(); i++) {
+            char charCurrent = string.charAt(i);
+            if(charCurrent == 'a' || charCurrent == 'e'|| charCurrent == 'i'|| charCurrent == 'o' || charCurrent == 'u') {
+            newString += (char)(charCurrent - 32); 
+            }   
+            else if (charCurrent >= 65 && charCurrent <= 90) {
+                newString += (char)(charCurrent + 32); 
+            } 
+            else {
+                newString += charCurrent;  
+            }
+        }
+        return newString;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
+        String newString = "";
+        int indexChar = 0;
+        for(int i = 0; i < string.length(); i++) {
+            if(string.charAt(i) >= 65 && string.charAt(i) <= 90) {
+                newString += (char)(string.charAt(i) + 32); 
+            } else if {
+                newString += string.charAt(i);
+            } else if (string.charAt(i) == 32 && string.charAt(i+1) >= 97 && string.charAt(i+1) <= 122) {
+                newString += (char)(string.charAt(i) - 32);
+            }
+            }
+            
+        }
         return "";
     }
 
